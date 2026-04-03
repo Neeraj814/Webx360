e858b1fdb5eb8c60a94fa86814d708705fac8159
Neeraj Kumar Bhoi <158065489+Neeraj814@users.noreply.github.com>
2026-04-03 12:48:14 +0530

    Update cloudinary.js

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // 🟢 Ise thoda lamba aur clear rakhein
-    api_key: process.env.CLOUDINARY_API_KEY,      
-    api_secret: process.env.CLOUDINARY_API_SECRET
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // 🟢 Render key se match kiya
+    api_key: process.env.CLOUDINARY_API_KEY,       // 🟢 Render key se match kiya
+    api_secret: process.env.CLOUDINARY_API_SECRET  // 🟢 Render key se match kiya
 });
 
 export default cloudinary;
