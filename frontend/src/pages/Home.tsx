8d43c06effea4d766439b8fb4841d5b4cd9e3982
Neeraj Kumar Bhoi <158065489+Neeraj814@users.noreply.github.com>
2026-09-15 15:21:07 +0530

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,33 +1,26 @@
 import Navbar from "@/components/shared/Navbar";
 import HeroSection from "@/components/HeroSection";
+import CompanyMarquee from "@/components/CompanyMarquee";
 import CategoryCarousel from "@/components/CategoryCarousel";
 import LatestJobs from "@/components/LatestJobs";
 import FeaturedJobs from "@/components/FeaturedJobs";
 import CTASection from "@/components/CTASection";
 import Footer from "@/components/Footer";
-import useGetAllJobs from "@/hooks/useGetAllJobs"; 
-import useGetAllCompanies from "@/hooks/useGetAllCompanies"; 
-import { useSelector } from "react-redux";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import useGetAllJobs from "@/hooks/useGetAllJobs";
+import useGetAllCompanies from "@/hooks/useGetAllCompanies";
 
 const Home = () => {
-  useGetAllJobs(); 
-  useGetAllCompanies(); 
-
-  const { user } = useSelector((store: any) => store.auth);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-  }, [user, navigate]);
+  useGetAllJobs();
+  useGetAllCompanies();
 
   return (
     <div className="min-h-screen bg-background">
       <Navbar />
       <HeroSection />
+      <CompanyMarquee />
       <CategoryCarousel />
       <LatestJobs />
-      <FeaturedJobs /> 
+      <FeaturedJobs />
       <CTASection />
       <Footer />
     </div>
